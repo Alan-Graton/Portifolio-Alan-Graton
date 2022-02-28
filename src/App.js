890e1b62153d566acb916f5eac49d7762fb3cbeb
Alan-Graton <graton.alan@gmail.com>
2022-02-28 10:53:01 -0300

    Initial settings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Gitlogo from "./Gitlogo.png";
 import "./App.css";
 
 // Landing Page
@@ -8,20 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={Gitlogo} className="App-logo" alt="logo" />
-        <p>Site Portifólio de Alan Graton Lourenço de Brito</p>
-        <a
-          className="App-link"
-          href="https://github.com/Alan-Graton"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Meu GitHub
-        </a>
-      </header>
-      <body bgcolor="grey">
         <Landing_INDEX />
-      </body>
+      </header>
     </div>
   );
 }
--- src/pages/LandingPage/LandingPage_Index.jsx
+++ src/pages/LandingPage/LandingPage_Index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 // Pages
 import Idioms_INDEX from "./Idioms/Idioms_Index";
@@ -7,19 +7,20 @@ import Langs_INDEX from "./ProgrammingLANGS/ProgrammingLANGS_Index";
 import Projects_INDEX from "./ProjectsDEV/ProjectsDEV_Index";
 import Social_INDEX from "./SocialMEDIA/SocialMEDIA_Index";
 
+// Components
+import Landing_AppBar from "./components/Landing_AppBar";
+
 function Landing_INDEX() {
-    return (
-        <div>
-            <h1>Landing Page</h1>
-            <ul>
-                <li><Idioms_INDEX /></li>
-                <li><ProfEXP_INDEX /></li>
-                <li><Langs_INDEX /></li>
-                <li><Projects_INDEX /></li>
-                <li><Social_INDEX /></li>
-            </ul>
-        </div>
-    );
+  return (
+    <>
+      <Landing_AppBar />
+      <Idioms_INDEX />
+      {/* <ProfEXP_INDEX />
+      <Langs_INDEX />
+      <Projects_INDEX />
+      <Social_INDEX /> */}
+    </>
+  );
 }
 
 export default Landing_INDEX;
