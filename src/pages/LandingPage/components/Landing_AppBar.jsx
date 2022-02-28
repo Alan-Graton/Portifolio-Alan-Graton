fd04e7ebd02e45c1dfeceb95e35b0921bf71e9f5
Alan-Graton <graton.alan@gmail.com>
2022-02-28 10:51:12 -0300

    App-bar Components Prototype

---
+++ src/pages/LandingPage/components/Landing_AppBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// AppBar Components
+import AppBar_Buttons from "./AppBar_Components/AppBar_Buttons";
+
+import "./styles/Appbar.css";
+
+function Landing_AppBar() {
+  return (
+    <div className="appBar">
+      <AppBar_Buttons />
+    </div>
+  );
+}
+
+export default Landing_AppBar;
