c90f73a613950f7ef06d4d89d350f1401884f30b
Alan-Graton <graton.alan@gmail.com>
2022-03-19 22:20:49 -0300

    Create 'Main' and 'Footer'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import "./App.css";
 
-// Landing Page
-import Landing_INDEX from "./pages/LandingPage/LandingPage_Index";
+// Header
+import NavBar from "./pages/LandingPage/components/Navbar/Navbar.jsx";
+
+// Body Components
+import Idioms_INDEX from "./pages/LandingPage/Idioms/Idioms_Index";
+// import ProfEXP_INDEX from "./pages/LandingPage/ProfessionalEXP/ProfessionalEXP_Index";
+// import Langs_INDEX from "./pages/LandingPage/ProgrammingLANGS/ProgrammingLANGS_Index";
+// import Projects_INDEX from "./pages/LandingPage/ProjectsDEV/ProjectsDEV_Index";
+// import Social_INDEX from "./pages/LandingPage/SocialMEDIA/SocialMEDIA_Index";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Landing_INDEX />
+        <NavBar />
       </header>
+      <main className="App-main">
+        <Idioms_INDEX />
+      </main>
+      <footer className="App-footer">
+        <p className="copyright">© Alan Graton Lourenço de Brito - 2022</p>
+      </footer>
     </div>
   );
 }
