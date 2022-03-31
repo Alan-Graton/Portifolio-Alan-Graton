33f45725ca88720e235f784c0d9fd1212a6d0914
Alan-Graton <graton.alan@gmail.com>
2022-03-31 10:38:09 -0300

    Adjustments in 'Idioms Page'

--- src/pages/LandingPage/Idioms/Idioms_Index.jsx
+++ src/pages/LandingPage/Idioms/Idioms_Index.jsx
@@ -10,31 +10,29 @@ import "./styles/idiomsGrid.css";
 
 function Idioms_INDEX() {
   return (
-    // <body>
-      <div className="container">
-        <div className="card">
-          <div className="card-content">
-            <img src={english} alt="USA Flag" />
-            <h5>Inglês - Fluente</h5>
-            <p>6 anos de estudo no CNA</p>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-content">
-            <img src={spanish} alt="Spain Flag" />
-            <h5>Espanhol - Intermediário</h5>
-            <p>2 anos de estudo no CNA</p>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-content">
-            <img src={german} alt="Germany Flag" />
-            <h5>Alemão - Iniciante</h5>
-            <p>Estudo em andamento. 1 ano de estudo no Instuto Goethe</p>
-          </div>
+    <div className="container">
+      <div className="card">
+        <div className="card-content">
+          <img src={english} alt="USA Flag" />
+          <h5>Inglês - Fluente</h5>
+          <p>6 anos de estudo no CNA</p>
         </div>
       </div>
-    // </body>
+      <div className="card">
+        <div className="card-content">
+          <img src={spanish} alt="Spain Flag" />
+          <h5>Espanhol - Intermediário</h5>
+          <p>2 anos de estudo no CNA</p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-content">
+          <img src={german} alt="Germany Flag" />
+          <h5>Alemão - Iniciante</h5>
+          <p>Estudo em andamento. 1 ano de estudo no Instituto Goethe</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
