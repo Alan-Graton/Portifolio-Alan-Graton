c736ef04c251c14a21f6d9d4c2cb7406a66641b4
Alan-Graton <graton.alan@gmail.com>
2022-03-31 10:36:15 -0300

    Minor Adjustments in 'App' and 'index' Main Files

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 // Header
 import NavBar from "./pages/LandingPage/components/Navbar/Navbar.jsx";
 
-// Body Components
+// Navbar Buttons
 import Idioms_INDEX from "./pages/LandingPage/Idioms/Idioms_Index";
 // import ProfEXP_INDEX from "./pages/LandingPage/ProfessionalEXP/ProfessionalEXP_Index";
-// import Langs_INDEX from "./pages/LandingPage/ProgrammingLANGS/ProgrammingLANGS_Index";
-// import Projects_INDEX from "./pages/LandingPage/ProjectsDEV/ProjectsDEV_Index";
+import Projects_INDEX from "./pages/LandingPage/ProjectsDEV/ProjectsDEV_Index";
 // import Social_INDEX from "./pages/LandingPage/SocialMEDIA/SocialMEDIA_Index";
+
+// Main Components
+import LandingPage from "./pages/LandingPage/index.jsx";
 
 function App() {
   return (
@@ -17,7 +19,9 @@ function App() {
         <NavBar />
       </header>
       <main className="App-main">
-        <Idioms_INDEX />
+        {/* <LandingPage /> */}
+        <Projects_INDEX />
+        {/* <Idioms_INDEX /> */}
       </main>
       <footer className="App-footer">
         <p className="copyright">© Alan Graton Lourenço de Brito - 2022</p>
