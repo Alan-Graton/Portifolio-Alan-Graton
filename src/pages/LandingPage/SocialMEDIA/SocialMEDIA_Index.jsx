32e40e5f2c949b4b459cfca5bb12e24df94b5b63
Alan-Graton <graton.alan@gmail.com>
2022-04-12 12:31:38 -0300

    Create 'Social Media' side card

--- src/pages/LandingPage/SocialMEDIA/SocialMEDIA_Index.jsx
+++ src/pages/LandingPage/SocialMEDIA/SocialMEDIA_Index.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 
+import "./styles/styles.css";
+
+// Logos
+import GitHub_Logo from "./images/GitHub_Logo.svg";
+import Instagram_Logo from "./images/Instagram_Logo.svg";
+import LinkedIN_Logo from "./images/LinkedIN_Logo.svg";
+
 function Social_INDEX() {
   return (
-    <div>
-      <h2>Redes Sociais</h2>
-      <ul>
-        <li>
-          <a href="https://www.instagram.com/graton.alan/">GitHub</a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/graton.alan">Instagram</a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/alan-graton-louren%C3%A7o-de-brito-2695571b5/">
-            LinkedIN
-          </a>
-        </li>
-      </ul>
-    </div>
+    <ul className="social-media-container">
+      <li className="social-media">
+        <a href="https://github.com/Alan-Graton">
+          <img className="social-media-logo" src={GitHub_Logo} alt="GitHub Logo" />
+        </a>
+      </li>
+      <li className="social-media">
+        <a href="https://www.instagram.com/graton.alan">
+          <img className="social-media-logo" src={Instagram_Logo} alt="Instagram Logo" />
+        </a>
+      </li>
+      <li className="social-media">
+        <a href="https://www.linkedin.com/in/alan-graton-louren%C3%A7o-de-brito-2695571b5/">
+          <img className="social-media-logo" src={LinkedIN_Logo} alt="LinkedIN Logo" />
+        </a>
+      </li>
+    </ul>
   );
 }
 
