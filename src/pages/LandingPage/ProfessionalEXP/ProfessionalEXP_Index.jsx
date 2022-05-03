a07a537a12366663f82c72419eef5496e4ed1d6e
Alan-Graton <graton.alan@gmail.com>
2022-05-03 14:02:34 -0300

    Fix "Professional Experience" page

--- src/pages/LandingPage/ProfessionalEXP/ProfessionalEXP_Index.jsx
+++ src/pages/LandingPage/ProfessionalEXP/ProfessionalEXP_Index.jsx
@@ -5,55 +5,71 @@ import "./styles/styles.css";
 import GI_Logo from "./images/GI_Logo.png";
 
 // Learned Techs Logos
-import sap_cpiLogo from "./images/sap-cpiLogo.svg";
+import javaScriptLogo from "./images/javaScriptLogo.svg";
+import test from "./images/jvsLogo.svg";
 import azureLogo from "./images/azureLogo.svg";
 import firebaseLogo from "./images/firebaseLogo.svg";
 import reactjsLogo from "./images/reactjsLogo.svg";
+import sap_cpiLogo from "./images/sap-cpiLogo.svg";
+import postmanLogo from "./images/postmanLogo.png";
 
 function ProfEXP_INDEX() {
   return (
     <div className="exp-container">
       <div className="company-title">
-        <div className="profExp-company">
-          <h1 className="company-name">Grupo Intelsis Brasil</h1>
-          <img
-            className="company-logo"
-            src={GI_Logo}
-            width="75px"
-            alt="Grupo Intelsis Brasil Logo"
-          />
-        </div>
+        <h1 className="company-name">Grupo Intelsis Brasil</h1>
+        <img
+          className="company-logo"
+          src={GI_Logo}
+          alt="Grupo Intelsis Brasil Logo"
+        />
       </div>
       <div className="profExp-activities">
         <div className="profExp-acivities-description">
           <h2>Tecnologias vistas</h2>
+          {/* Descrição das Experiências Profissionais */}
           <p className="profExp-acivities-description-paragraph">
             Foram realizados alguns "desafios" no começo no Estágio para que os
             recrutadores pudessem saber onde o estagiário se encaixaria melhor.
             <br />
             Os desafios/projetos foram:
             <br />
-            <img src={azureLogo} alt="Azure Logo" />
-            Escrever um script utiilizando JavaScript para se conectar ao Banco
-            de Dados da empresa no Azure e retornar o Payload de uma certa
-            tabela.
-            <br />
-            <img className="firebase-logo" src={firebaseLogo} alt="Firebase Logo" />
-            <img src={reactjsLogo} alt="ReactJS Logo" />
-            Desenvolvido individualmente um portal de
-            <strong> apontamento de horas trabalhadas </strong>
-            {"(sem ajuda técnica)"}. Foi utilizado ReactJS
-            <strong>{"(obrigatório)"}</strong>, Material-UI
-            <strong>{"(opcional)"}</strong> e Firebase
-            <strong>{"(obrigatório)"}</strong>.
-            <br />
-            <img src={sap_cpiLogo} alt="SAP CPI Logo" />
-            Ter a primeira experiência como Consultor CPI em um projeto para a
-            LATAM, onde foi necessário trabalhar com funcionários de outras
-            empresas{"(ECentaCloud)"}, sendo eles do exterior
-            {"(Chile, EUA e Índia)"}.
-            <br />
           </p>
+          {/* Experiências Profissionais */}
+          <section>
+          <hr style={{ opacity: "10%" }} />
+            <img src={azureLogo} alt="Azure Logo" />
+            <img src={test} alt="JavaScript Logo" />
+            <p>
+              Escrever um script utiilizando JavaScript para se conectar ao
+              Banco de Dados da empresa no Azure e retornar o Payload de uma
+              certa tabela.
+            </p>
+          </section>
+          <section>
+          <hr style={{ opacity: "10%" }} />
+            <img src={firebaseLogo} alt="Firebase Logo" />
+            <img src={reactjsLogo} alt="ReactJS Logo" />
+            <p>
+              Desenvolvido individualmente um portal de
+              <strong> apontamento de horas trabalhadas </strong>
+              {"(sem ajuda técnica)"}. Foi utilizado ReactJS
+              <strong>{"(obrigatório)"}</strong>, Material-UI
+              <strong>{"(opcional)"}</strong> e Firebase
+              <strong>{"(obrigatório)"}</strong>.
+            </p>
+          </section>
+          <section>
+          <hr style={{ opacity: "10%" }} />
+            <img src={sap_cpiLogo} alt="SAP Logo" />
+            <img src={postmanLogo} alt="Postman Logo" />
+            <p>
+              Ter a primeira experiência como Consultor CPI em um projeto para a
+              LATAM, onde foi necessário trabalhar com funcionários de outras
+              empresas{"(ECentaCloud)"}, sendo eles do exterior
+              {"(Chile, EUA e Índia)"}.
+            </p>
+          </section>
         </div>
       </div>
     </div>
